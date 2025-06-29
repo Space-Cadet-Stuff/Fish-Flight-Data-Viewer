cbc61ed6eac10cec8b2e4a08f8ef90c794c2ca28
Space-Cadet-Stuff <jfisher1@student.saintaug.nsw.edu.au>
2025-06-29 22:33:25 +1000

    added 404 and 500 Error handling pages

--- app.py
+++ app.py
@@ -325,5 +325,18 @@ def jakito():
 
     return render_template("jakito.html", sacrificed=sacrificed, has_florp=has_florp)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/cause-error')
+def cause_error():
+    raise Exception("Intentional server error for testing")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
